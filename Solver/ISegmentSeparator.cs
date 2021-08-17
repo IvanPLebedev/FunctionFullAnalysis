9dce5125b3143d8c140ab2801f22480d2aaf1f9f
Ivan <ivan.p.lebedev@ynadex.ru>
2021-08-17 21:22:39 +0400

    brought beauty

---
+++ Solver/ISegmentSeparator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using FunctionFullAnalysis.Utils;
+
+namespace FunctionFullAnalysis.Solver
+{
+    interface ISegmentSeparator
+    {
+        IEnumerable<Segment> GetSegmentsWithSolution(Expression<Func<double, double>> function, Segment domain, double step);
+    }
+}
--- Solver/SegmentSeparatorWithSolution.cs
+++ Solver/SegmentSeparatorWithSolution.cs
@@ -1,26 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Linq.Expressions;
 using FunctionFullAnalysis.Utils;
 
 namespace FunctionFullAnalysis.Solver
 {
-    class SegmentSeparatorWithSolution
+    class SegmentSeparatorWithSolution : ISegmentSeparator
     {
-        public static IEnumerable<Segment> GetSegmentsWithSolution(Expression<Func<double, double>> function, Segment domain, double step)
+        public IEnumerable<Segment> GetSegmentsWithSolution(Expression<Func<double, double>> function, Segment domain, double step)
         {
+            var f = function.Compile();
             var a1 = domain.A;
             var b1 = a1 + step;
-            while (b1 < domain.B)
+            do
             {
-                yield return new Segment(a1, b1);
+                if (f(a1) * f(b1) < 0)
+                    yield return new Segment(a1, b1);
                 a1 = b1;
                 b1 = a1 + step;
             }
-            yield return new Segment(a1, domain.B);
+            while (b1 < domain.B);  
         }
     }
 }
