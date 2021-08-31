0ec46d9e0c6c72794121c3f9627e11d13f676bf1
Ivan <ivan.p.lebedev@ynadex.ru>
2021-08-31 20:18:27 +0400

    Add SimpleExpressionParser

---
+++ Tests/SimpleExpressionParserTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq.Expressions;
+using FunctionFullAnalysis.Parsers;
+
+namespace FunctionFullAnalysis.Tests
+{
+    [TestFixture]
+    class SimpleExpressionParserTests
+    {
+        private SimpleExpressionParser parser = new SimpleExpressionParser();
+
+        [Test]
+        public void Test1()
+        {
+            var str = "x+2";
+            var func = parser.ParseExpression(str).Compile();
+            var res = func(2);
+        }
+    }
+}
